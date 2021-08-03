5b24354f376a3bca4fd23eac6d48824f51327fea
Kelsey Deffendol <kelsey@deffendol.org>
2021-08-02 22:24:29 -0600

    Add Truck class

---
+++ Truck.py
@@ -0,0 +1,14 @@
+import PackageRepository
+
+class Truck:
+    def __init__(self, capacity, speed):
+        self.packages = PackageRepository
+        self.capacity = capacity
+        self.speed = speed
+
+    def load_package(self, package):
+        self.packages.insert(package)
+
+    def remove_package(self, package_id):
+        self.package.remove()
+        
