06cb4e9179ce82fabc71a9bfd42fea380f118fdc
Kelsey Deffendol <kelsey@deffendol.org>
2022-01-19 21:27:07 -0700

    Update Package class to match csv data

--- Package.py
+++ Package.py
@@ -2,11 +2,11 @@ class Package:
     """
     Package class
     """
-    def __init__(self, id, address, deadline, city, zipcode, weight, status):
+    def __init__(self, id, address, deadline, city, zipcode, weight, notes):
         self.id = id
         self.address = address
-        self.deadline = deadline
         self.city = city
         self.zipcode = zipcode
+        self.deadline = deadline
         self.weight = weight
-        self.status = status
+        self.notes = notes
